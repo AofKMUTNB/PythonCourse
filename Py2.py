a54aff462e1c653a002e0525104f5c8c1bd08de1
AofKMUTNB <s6103081610247@email.kmutnb.ac.th>
2023-02-26 21:35:31 +0700

    update

--- Py2.py
+++ Py2.py
@@ -5,4 +5,12 @@ friend = 'สมชาย'
 print('สวัสดี'+friend)
 money = 10
 
-print(friend + 'ยืมเงิน' + str(money) + ' บาท')   #เนื่องจากmoneyประเภทเป็นintจึงจำเป็นต้องแปลงเป็นข้อความ(str)ก่อน
+print(friend + 'ยืมเงิน ' + str(money) + ' บาท')   #เนื่องจากmoneyประเภทเป็นintจึงจำเป็นต้องแปลงเป็นข้อความ(str)ก่อน
+print('{}ยืมเงิน {} บาท'.format(friend,money))      #ตัวแปรFriend,maneyในformatจะถูกนำไปใส่ในปีกกาทั้ง2
+print(f'{friend}ยืมเงิน {money} บาท')              # f มาจากคำว่า format
+
+money = 14500020492
+print(f'{money:,.2f}')
+
+import math
+math.pi
