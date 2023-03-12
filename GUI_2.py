a6d35eda98fa3f2f2b7ff4d90b90d820d122ae50
AofKMUTNB <s6103081610247@email.kmutnb.ac.th>
2023-03-12 22:45:34 +0700

    Update GUI_2

--- GUI_2.py
+++ GUI_2.py
@@ -15,4 +15,16 @@ LB1.pack(ipadx=10,ipady=5)
 FB2 =Frame(GUI)
 FB2.place(x=100,y=80)
 
+def Button1() :
+    formula = str()
+    text= 'You can do it'
+    messagebox.showinfo('let do it',text)
+
+FB1 = Frame(GUI)
+FB1.place(x=295,y=220)
+B2 = ttk.Button(FB1,text='Do it',command=Button1)
+B2.pack(ipadx=10,ipady=15)
+
+Box1 = ttk.Entry(LB1)
+Box1.grid(row=2,column=0,padx=20,pady=20)
 GUI.mainloop()
