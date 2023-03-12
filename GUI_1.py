09dba1ebe6daba2ebd96fcdb9b1fc24d40709672
AofKMUTNB <s6103081610247@email.kmutnb.ac.th>
2023-03-12 20:42:56 +0700

    Update

--- GUI_1.py
+++ GUI_1.py
@@ -15,6 +15,15 @@ L1.grid(row=0,column=0,columnspan=2)
 
 def Button1():
     text = 'ตอนนี้มีเงินในบัญชี  ' + str(int(E1.get()) - int(E2.get())) + 'บาท'
+    messagebox.showinfo('เงินในบัญชี',text)
+    
+L2 = Label(GUI,text='รายได้', font=('Angsana New',15),fg='black')
+L2.grid(row=1,column=0,padx=20,pady=20,sticky='W')
 
+E1 = Entry(GUI)
+E1.grid(row=1,column=1,padx=20,pady=20)
+
+L3 = Label(GUI,text='รายจ่าย', font=('Angsana New',15),fg='black')
+L3.grid(row=2,column=1,padx=20,pady=20,sticky='W')
 
 GUI.mainloop()
