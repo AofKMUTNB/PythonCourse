133c2299b225cade78f435b6428afcc7b19bd2ca
AofKMUTNB <s6103081610247@email.kmutnb.ac.th>
2023-03-24 14:05:07 +0700

    Up

--- ML.py
+++ ML.py
@@ -1,6 +1,4 @@
-from sklearn import tree
-features = [[120,5] , [110,5] , [180,12] , [163,12]]
-labels = ["ECO Car / B-Segment" , "ECO Car / B-Segment" , "VAN" , "VAN"]
-classifier = tree.DecisionTreeClassifier()
-classifier = classifier.fit(features,labels)
-print(classifier.predict([[150 , 12]]))
+from sklearn import datasets
+
+iris_dataset = datasets.load_iris()
+print(iris_dataset['target_names'])
